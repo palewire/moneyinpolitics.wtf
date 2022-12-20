3dc04ca62a1134da8a51abb4a18d2666f5f2067b
Ben Welsh <b@palewi.re>
2022-12-20 13:58:15 +0000

    A little more style

--- scripts/components/Card.svelte
+++ scripts/components/Card.svelte
@@ -1,4 +1,7 @@
 <script>
+  import Share from "svelte-material-icons/Share.svelte";
+  import Flag from "svelte-material-icons/Flag.svelte";
+
   /**
    * @type {string}
    */
@@ -7,8 +10,29 @@
    * @type {string}
    */
    export let type;
+  /**
+   * @type {Array<any>}
+   */
+   export let definitionList;
 </script>
 
 <div class="card">
-    <div class="card--headline">{ word } <span class="card--type">{ type }</span></div>
+    <div class="card--headline"><dt class="word">{ word }</dt> <span class="card--type">{ type }</span></div>
+    <div class="card--body">
+      <dd class="card--definition-list">
+        <div class="card--definition">
+          {#each definitionList as definition}
+            <div class="card--definition-text">{ definition.text }</div>
+            <!-- <div class="card--definition-in-use">{ definition.in_use }</div> -->
+          {/each}
+        </div>
+      </dd>
+      <div class="card--actions">
+        <div class="card--action-item card--expand">Expand</div>
+        <div class="card--supplemental">
+          <div class="card--action-item card--flag"><Flag size=15 /></div>
+          <div class="card--action-item card--share"><Share size=15 /></div>
+        </div>
+      </div>
+    </div>
 </div>
--- scripts/components/CardList.svelte
+++ scripts/components/CardList.svelte
@@ -2,13 +2,13 @@
   import Card from './Card.svelte'; 
 
   /**
-   * @type {Array<Object>}
+   * @type {Array<any>}
    */
    export let dictionary;
 </script>
 
-<section class="card-list">
+<dl class="card-list">
   {#each dictionary as obj}
-    <Card word={obj.word} type={obj.type}></Card>
+    <Card word={obj.word} type={obj.type} definitionList={ obj.definition_list }></Card>
   {/each}
-</section>
+</dl>

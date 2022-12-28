20fe73df68e2265672d3fd238a404e8cd30495c8
Ben Welsh <b@palewi.re>
2022-12-28 01:10:18 +0000

    Very basic search

--- scripts/components/CardList.svelte
+++ scripts/components/CardList.svelte
@@ -1,18 +1,29 @@
 <script>
+  import { writable, derived } from 'svelte/store';
   import Card from './Card.svelte'; 
 
   /**
    * @type {Array<any>}
    */
    export let dictionary;
+
+  export const term = writable('');
+  export const items = writable(dictionary);
+  export const filtered = derived(
+    [term, items], 
+    ([$term, $items]) => $items.filter(x => x.word.toLowerCase().includes($term.toLowerCase()))
+  );
+
+  let val = '';
+  $: term.set(val);
 </script>
 
 <div class="search">
-  <input type="text">
+  <input bind:value={val} type="text" placeholder="Search a word...">
 </div>
 
 <dl class="card-list">
-  {#each dictionary as obj}
+  {#each $filtered as obj}
     <Card word={obj.word} type={obj.type} definitionList={ obj.definition_list }></Card>
   {/each}
 </dl>

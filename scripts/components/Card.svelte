189606175a0e942d5a6eef71031246ade03279d6
palewire <b@palewi.re>
2023-07-03 15:38:56 -0400

    Fixes #115

--- scripts/components/Card.svelte
+++ scripts/components/Card.svelte
@@ -22,7 +22,7 @@
    */
    export let definitionList;
 
-  const truncate = (inputString, maximumLength = 44) => {
+  const truncate = (inputString, maximumLength = 50) => {
     let outputString = "";
     let outputLength = 0;
     const tokenList = inputString.trim().split(' ');
